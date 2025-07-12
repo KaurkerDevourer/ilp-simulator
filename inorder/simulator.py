a40a1340be42e3413fd62bc6037190534c078386
Stepan Styopkin <smstyopkin@edu.hse.ru>
2025-07-12 23:19:29 +0000

    tracing + jumps

--- inorder/simulator.py
+++ inorder/simulator.py
@@ -1,4 +1,5 @@
 import re
+import copy
 from dataclasses import dataclass
 from typing import List, Dict, Optional
 
@@ -7,6 +8,7 @@ CPU_CONFIG = {
         'ALU': 2,
         'MUL_DIV': 1,
         'LOAD_STORE': 1,
+        'INF': 1000000,
     },
     'latency': {
         'add': 1,
@@ -16,23 +18,53 @@ CPU_CONFIG = {
         'mul': 3,
         'muli': 3,
         'div': 10,
-        'divi': 10,
+        'divi': 4,
+        'and': 1,
+        'andi': 1,
+        'or': 1,
+        'ori': 1,
+        'xor': 1,
+        'xori': 1,
+        'shr': 1,
+        'shl': 1,
         'lw': 5,
         'li': 5,
-        'sw': 5
+        'sw': 5,
+        'jne': 1,
+        'jie': 1,
+        'jgt': 1,
+        'jge': 1,
+        'jle': 1,
+        'jlt': 1,
+        'jmp': 1
     },
     'op_to_unit': {
         'add': 'ALU',
         'addi': 'ALU',
         'sub': 'ALU',
         'subi': 'ALU',
+        'shl': 'ALU',
+        'shr': 'ALU',
         'mul': 'MUL_DIV',
         'muli': 'MUL_DIV',
         'div': 'MUL_DIV',
         'divi': 'MUL_DIV',
+        'and': 'ALU',
+        'andi': 'ALU',
+        'or': 'ALU',
+        'ori': 'ALU',
+        'xor': 'ALU',
+        'xori': 'ALU',
         'lw': 'LOAD_STORE',
         'li': 'ALU',
-        'sw': 'LOAD_STORE'
+        'sw': 'LOAD_STORE',
+        'jne': 'INF',
+        'jie': 'INF',
+        'jgt': 'INF',
+        'jge': 'INF',
+        'jle': 'INF',
+        'jlt': 'INF',
+        'jmp': 'INF'
     }
 }
 
@@ -45,6 +77,7 @@ class Instruction:
     imm: Optional[int] = None
     offset: int = 0
     status: str = "WAITING"
+    target: str = ""
     start_cycle: int = -1
     complete_cycle: int = -1
     result_value: Optional[int] = None
@@ -60,6 +93,7 @@ class ILPSimulator:
         self.total_cycles: int = 0
 
         self.program: List[Instruction] = []
+        self.labels: Dict[str, int] = {}
         
         self.registers: Dict[str, int] = {}
         self.memory: Dict[int, int] = {}
@@ -71,10 +105,27 @@ class ILPSimulator:
         self.register_writeback_cycle: Dict[str, int] = {}
 
     def load(self, file):
+        instruction_index = 0
+        with open(file) as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                if line.endswith(':'):
+                    label_name = line[:-1].strip()
+                    if label_name in self.labels:
+                        raise ValueError(f"Дублирующаяся метка: {label_name}")
+                    self.labels[label_name] = instruction_index
+                else:
+                    instruction_index += 1
+
         r_type_regex = re.compile(r"^\s*(\w+)\s+(\w+),\s*(\w+),\s*(\w+)\s*$")
         mem_type_regex = re.compile(r"^\s*(\w+)\s+(\w+),\s*(-?\d+)\((\w+)\)\s*$")
         imm_type_regex = re.compile(r"^\s*(\w+)\s+(\w+),\s*(\w+),\s*#(-?\d+)\s*$")
         loadi_regex = re.compile(r"^\s*(\w+)\s+(\w+),\s*#(-?\d+)\s*$")
+        branch_regex = re.compile(r"^\s*(jie|jne|jge|jgt|jlt|jle)\s+(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*$")
+        jmp_regex = re.compile(r"^\s*(jmp)\s+(\w+)\s*$")
+        shift_regex = re.compile(r"^\s*(shl|shr)\s+(\w+),\s*#(\d+)\s*$")
 
         with open(file) as f:
             for i, line in enumerate(f):
@@ -83,8 +134,26 @@ class ILPSimulator:
                 mem_match = mem_type_regex.match(line)
                 imm_match = imm_type_regex.match(line)
                 loadi_match = loadi_regex.match(line)
+                branch_match = branch_regex.match(line)
+                jmp_match = jmp_regex.match(line)
+                shift_match = shift_regex.match(line)
 
-                if r_match:
+                if shift_match:
+                    op, rd, imm_str = shift_match.groups()
+                    if op not in self.config['op_to_unit']:
+                        raise ValueError(f"Строка {i}: Недоступная операция '{op}' в {line}")
+                    self.program.append(Instruction(op=op, rd=rd, rs1=rd, imm=int(imm_str), raw_text=line))
+                elif jmp_match:
+                    op, target_label = jmp_match.groups()
+                    if target_label not in self.labels:
+                        raise ValueError(f"Строка {i+1}: Неизвестная метка '{target_label}'")
+                    self.program.append(Instruction(op=op, target=target_label, raw_text=line))
+                elif branch_match:
+                    op, rs1, rs2, target_label = branch_match.groups()
+                    if target_label not in self.labels:
+                        raise ValueError(f"Строка {i+1}: Неизвестная метка '{target_label}'")
+                    self.program.append(Instruction(op=op, rs1=rs1, rs2=rs2, target=target_label, raw_text=line))
+                elif r_match:
                     op, rd, rs1, rs2 = r_match.groups()
                     if op not in self.config['op_to_unit']:
                         raise ValueError(f"Строка {i}: Недоступная операция '{op}' в {line}")
@@ -103,10 +172,9 @@ class ILPSimulator:
                     self.program.append(Instruction(op=op, rd=reg1, rs1=reg2, offset=offset, raw_text=line))
                 elif imm_match:
                     op, rd, rs1, imm_str = imm_match.groups()
-                    imm = int(imm_str)
                     if op not in self.config['op_to_unit']:
                         raise ValueError(f"Строка {i}: Недоступная операция '{op}' в {line}")
-                    self.program.append(Instruction(op=op, rd=rd, rs1=rs1, imm=imm, raw_text=line))
+                    self.program.append(Instruction(op=op, rd=rd, rs1=rs1, imm=int(imm_str), raw_text=line))
                 elif loadi_match:
                     op, rd, imm_str = loadi_match.groups()
                     imm = int(imm_str)
@@ -123,7 +191,11 @@ class ILPSimulator:
 
         total_time = 0
         last_time = 0
-        for instr in self.program:
+
+        self.execution_trace: List[Instruction] = []
+        pc = 0
+        while pc < len(self.program):
+            instr = self.program[pc]
             source_regs = [r for r in [instr.rs1, instr.rs2, instr.rd if instr.op == 'sw' else None] if r]
             data_ready_at = max([register_ready_at.get(r, 0) for r in source_regs], default=0)
 
@@ -134,26 +206,48 @@ class ILPSimulator:
 
             start_time = max(data_ready_at, earliest_unit_free_at, last_time) + 1
             last_time = start_time
-            
+
             latency = self.config['latency'][instr.op]
             end_time = start_time + latency - 1
 
             instr.start_cycle = start_time
             instr.complete_cycle = end_time
+            self.execution_trace.append(copy.copy(instr))
 
             if instr.rd and instr.op != 'sw':
                 register_ready_at[instr.rd] = end_time
 
             units_free_at[unit_type][unit_index_to_use] = end_time
-            
+
             total_time = max(total_time, end_time)
 
             self.compute_and_commit(instr)
+
+            next_pc = pc + 1
+            if instr.op == 'jmp':
+                next_pc = self.labels[instr.target]
+            elif instr.op == 'jie' and self.registers.get(instr.rs1, 0) == self.registers.get(instr.rs2, 0):
+                next_pc = self.labels[instr.target]
+            elif instr.op == 'jne' and self.registers.get(instr.rs1, 0) != self.registers.get(instr.rs2, 0):
+                next_pc = self.labels[instr.target]
+            elif instr.op == 'jgt' and self.registers.get(instr.rs1, 0) > self.registers.get(instr.rs2, 0):
+                next_pc = self.labels[instr.target]
+            elif instr.op == 'jge' and self.registers.get(instr.rs1, 0) >= self.registers.get(instr.rs2, 0):
+                next_pc = self.labels[instr.target]
+            elif instr.op == 'jlt' and self.registers.get(instr.rs1, 0) < self.registers.get(instr.rs2, 0):
+                next_pc = self.labels[instr.target]
+            elif instr.op == 'jle' and self.registers.get(instr.rs1, 0) <= self.registers.get(instr.rs2, 0):
+                next_pc = self.labels[instr.target]
+
+            pc = next_pc
 
         self.total_cycles = total_time
 
     def compute_and_commit(self, instr):
         op = instr.op
+
+        if op[0] == 'j' and len(op) == 3:
+            return
 
         val1 = self.registers.get(instr.rs1, 0)
         val2 = self.registers.get(instr.rs2, 0)
@@ -162,6 +256,10 @@ class ILPSimulator:
 
         if op == 'li':
             result = instr.imm
+        elif op == 'shl':
+            result = val1 << instr.imm
+        elif op == 'shr':
+            result = val1 >> instr.imm
         elif op == 'addi':
             result = val1 + instr.imm
         elif op == 'subi':
@@ -170,6 +268,12 @@ class ILPSimulator:
             result = val1 * instr.imm
         elif op == 'divi':
             result = val1 // instr.imm
+        elif op == 'andi':
+            result = val1 & instr.imm
+        elif op == 'ori':
+            result = val1 | instr.imm
+        elif op == 'xori':
+            result = val1 ^ instr.imm
         elif op == 'lw':
             base_addr = self.registers.get(instr.rs1, 0)
             address = base_addr + instr.offset
@@ -182,6 +286,12 @@ class ILPSimulator:
             result = val1 * val2
         elif op == 'div':
             result = val1 // val2
+        elif op == 'and':
+            result = val1 & val2
+        elif op == 'or':
+            result = val1 | val2
+        elif op == 'xor':
+            result = val1 ^ val2
         elif op == 'sw':
             pass 
         else:
@@ -193,7 +303,7 @@ class ILPSimulator:
             value_to_store = self.registers.get(instr.rs1, 0)
             self.memory[address] = value_to_store
         elif instr.rd:
-                self.registers[instr.rd] = result
+            self.registers[instr.rd] = result
 
     def print_timeline(self):
         header_text = "Результаты симуляции (Таймлайн)"
@@ -227,5 +337,16 @@ class ILPSimulator:
                 f"| {instr.start_cycle - 1:>5} "
                 f"| {instr.complete_cycle - 1:>5} "
                 f"| {timeline_str}")
+
+        print(f"\nИТОГО ЦИКЛОВ: {self.total_cycles}")
+
+    def print_trace(self):
+        header_text = "Результаты симуляции (Трейс)"
+        width = 80
+        print(f"\n{'=' * width}")
+        print(f"{header_text.center(width)}")
+        print(f"{'=' * width}")
+        for instr in self.execution_trace:
+            print(f"[{instr.start_cycle:03d}-{instr.complete_cycle:03d}] {instr.raw_text}")
 
         print(f"\nИТОГО ЦИКЛОВ: {self.total_cycles}")
--- inorder/test/test_inorder.py
+++ inorder/test/test_inorder.py
@@ -12,13 +12,26 @@ def test_load(tmp_path):
     subi t4, t2, #3
     muli t5, t3, #2
     divi t6, t5, #4
+    shl t6, #2
+    shr t6, #3
+    xor t1, t3, t2
+    ori t3, t4, #5
     li t7, #42
+    loop:
+    jmp loop
+    jne t3, t0, loop
+    jie t3, t0, loop
+    jge t3, t0, end
+    jle t3, t0, loop
+    jgt t3, t0, end
+    jlt t3, t0, loop
+    end:
     """
     p.write_text(test_code)
     sim = ILPSimulator()
     sim.load(str(p))
 
-    assert len(sim.program) == 8
+    assert len(sim.program) == 19
     assert sim.program[0].op == 'lw'
     assert sim.program[0].rd == 't1'
     assert sim.program[1].op == 'add'
@@ -37,8 +50,42 @@ def test_load(tmp_path):
     assert sim.program[6].op == 'divi'
     assert sim.program[6].rs1 == 't5'
     assert sim.program[6].imm == 4
-    assert sim.program[7].op == 'li'
-    assert sim.program[7].imm == 42
+    assert sim.program[7].op == 'shl'
+    assert sim.program[7].rd == 't6'
+    assert sim.program[7].imm == 2
+    assert sim.program[8].op == 'shr'
+    assert sim.program[8].rd == 't6'
+    assert sim.program[8].imm == 3
+    assert sim.program[9].op == 'xor'
+    assert sim.program[9].rd == 't1'
+    assert sim.program[9].rs1 == 't3'
+    assert sim.program[9].rs2 == 't2'
+    assert sim.program[10].op == 'ori'
+    assert sim.program[10].rd == 't3'
+    assert sim.program[10].rs1 == 't4'
+    assert sim.program[10].imm == 5
+    assert sim.program[11].op == 'li'
+    assert sim.program[11].imm == 42
+
+    assert len(sim.labels) == 2
+    assert sim.labels['loop'] == 12
+    assert sim.labels['end'] == 19
+    assert sim.program[12].op == 'jmp'
+    assert sim.program[12].target == 'loop'
+    assert sim.program[13].op == 'jne'
+    assert sim.program[13].rs1 == 't3'
+    assert sim.program[13].rs2 == 't0'
+    assert sim.program[13].target == 'loop'
+    assert sim.program[14].op == 'jie'
+    assert sim.program[14].target == 'loop'
+    assert sim.program[15].op == 'jge'
+    assert sim.program[15].target == 'end'
+    assert sim.program[16].op == 'jle'
+    assert sim.program[16].target == 'loop'
+    assert sim.program[17].op == 'jgt'
+    assert sim.program[17].target == 'end'
+    assert sim.program[18].op == 'jlt'
+    assert sim.program[18].target == 'loop'
 
 def test_all_arithmetic_ops(tmp_path):
     d = tmp_path
@@ -54,6 +101,10 @@ def test_all_arithmetic_ops(tmp_path):
     subi t7, t0, #2
     muli s0, t1, #4
     divi s1, t0, #2
+    li s3, #20
+    li s4, #20
+    shl s3, #1
+    shr s4, #2
     """
     p.write_text(test_code)
     sim = ILPSimulator()
@@ -69,6 +120,9 @@ def test_all_arithmetic_ops(tmp_path):
     assert sim.registers['t7'] == 18
     assert sim.registers['s0'] == 20
     assert sim.registers['s1'] == 10
+
+    assert sim.registers['s3'] == 40
+    assert sim.registers['s4'] == 5
 
 def test_run(tmp_path):
     d = tmp_path
@@ -91,3 +145,27 @@ def test_run(tmp_path):
     assert sim.memory[104] == 20
     assert sim.total_cycles == 11
     sim.print_timeline()
+
+def test_cycle_max(tmp_path):
+    d = tmp_path
+    p = d / "test_program.misc"
+
+    test_code = """li maxx, #0
+    li t3, #0
+    loop:
+    lw t4, 0(t1)
+    jle t4, maxx, skip_upd
+    addi maxx, t4, #0
+    skip_upd:
+    addi t1, t1, #4
+    addi t3, t3, #1
+    jne t3, t0, loop
+    """
+    p.write_text(test_code)
+    sim = ILPSimulator()
+    sim.load(str(p))
+    sim.registers = {'t0': 3, 't1': 100}
+    sim.memory = {100: 20, 104: 30, 108: 10}
+    sim.run()
+    assert sim.registers['maxx'] == 30
+    sim.print_trace()
